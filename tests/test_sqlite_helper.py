49e7839f042c5b9fbe733bf2143824d47a082e30
Luis Lemus <lemux.one@gmail.com>
2023-03-10 18:36:08 -0500

    add tests for sqlite db helper

---
+++ tests/test_sqlite_helper.py
@@ -0,0 +1,37 @@
+import unittest
+from db.sqlite import SqliteHelper
+
+class TestSqliteHelper(unittest.TestCase):
+
+    def setUp(self):
+        self.helper = SqliteHelper()
+    
+    def tearDown(self):
+        self.helper.close()
+
+
+    def test_count_tables(self):
+        sql = '''select count(*) as total_tables from sqlite_master;'''
+        ok, rows = self.helper.query(sql)
+        self.assertTrue(ok)
+        self.assertEqual(len(rows), 1)
+        self.assertTrue('total_tables' in rows[0].keys())
+        self.assertTrue(isinstance(rows[0]['total_tables'], int))
+
+
+    def test_create_table(self):
+        sql = '''CREATE TABLE "dummy" (
+            "id" INTEGER NOT NULL,
+            "name" VARCHAR(50),
+            PRIMARY KEY ("id")
+        );'''
+        before = self.__count_tables()
+        ok = self.helper.exec(sql)
+        self.assertTrue(ok)
+        after = self.__count_tables()
+        self.assertEqual(after, before + 1)
+    
+
+    def __count_tables(self):
+        _, rows = self.helper.query('select count(*) as count from sqlite_master;')
+        return rows[0]['count']
