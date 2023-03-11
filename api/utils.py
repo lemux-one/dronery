99dcfd9130d08d678f711c2a2b99f3a3f7f82b44
Luis Lemus <lemux.one@gmail.com>
2023-03-10 20:25:12 -0500

    improve response handling functions

--- api/utils.py
+++ api/utils.py
@@ -2,20 +2,41 @@ import json
 from http import HTTPStatus
 from bottle import response
 
-def json_response(payload: dict, status_code: HTTPStatus = HTTPStatus.OK) -> str:
+
+def json_response(
+        payload: dict, 
+        status_code: HTTPStatus
+    ) -> str:
     '''
-    Prepares a stringified JSON response object based on the given payload.
-    If not status_code is given then '200 OK' is assumed.
+    Prepares a stringified JSON response object.
     '''
     response.headers['Content-Type'] = 'application/json'
     response.status = status_code
     return json.dumps(payload)
 
 
-def error_response(reason: str, status_code: HTTPStatus) -> str:
+def data_response(
+        data: dict, 
+        status_code: HTTPStatus = HTTPStatus.OK
+    ) -> str:
+    '''
+    Prepares a stringified JSON response to wrap data.
+    '''
+    return json_response(
+        payload={
+            'status': status_code,
+            'data': data
+        }, 
+        status_code=status_code
+    )
+
+
+def error_response(
+        reason: str = 'Internal server error', 
+        status_code: HTTPStatus = HTTPStatus.INTERNAL_SERVER_ERROR
+    ) -> str:
     '''
     Prepares a stringified JSON response to notify errors or exceptions.
-    A descriptive 'reason' message and a corresponding 'HTTP status' are required.
     '''
     return json_response(
         payload={
