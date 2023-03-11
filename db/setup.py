6890fc0eb014197efce447eac7fe72afd0f16198
Luis Lemus <lemux.one@gmail.com>
2023-03-11 04:18:58 -0500

    raise error if migrations failed

--- db/setup.py
+++ db/setup.py
@@ -1,11 +1,20 @@
 from db.db_helper import DbHelper
 
+class MigrationsException(Exception):
+    def __str__(self) -> str:
+        return "ERR - An error ocurred while running migrations. See log for details."
+
 def run_migrations(dbhelper: DbHelper) -> None:
+    '''
+    Runs every statement found in "migrations.sql" one by one.
+    This function is meant to be called before anything else in
+    order to provide initial database structure and data.
+    '''
     print('INF - Running migrations using DbHelper: ' + str(dbhelper))
     script = ""
     statements = []
-    with open('migrations.sql', 'r') as file:
-        while line := file.readline():
+    with open('migrations.sql', mode='r', encoding='utf-8') as file:
+        for line in file:
             if line.endswith(';\n'):
                 statements.append(script + line)
                 script = ""
@@ -17,4 +26,4 @@ def run_migrations(dbhelper: DbHelper) -> None:
     if ok:
         print('INF - Migrations applied!')
     else:
-        print('ERR - Failed to apply migrations. See log for details.')
+        raise(MigrationsException())
