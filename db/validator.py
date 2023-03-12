881cebaf5fef7777a08948bc0135d051a56ffea7
Luis Lemus <lemux.one@gmail.com>
2023-03-12 19:58:21 -0400

    make validators more flexible and adjust tests

--- db/validator.py
+++ db/validator.py
@@ -1,31 +1,31 @@
 #
 #   Common validators
 #
-def validate_int(value, min: float = None, max: float = None) -> (bool, str):
+def validate_int(field, value) -> (bool, str):
     if not isinstance(value, int):
         return False, 'Integer only'
-    if min and value < min:
-        return False, f'Minimum of {min}'
-    if max and value > max:
-        return False, f'Maximum of {max}'
+    if field.min and value < field.min:
+        return False, f'Minimum of {field.min}'
+    if field.max and value > field.max:
+        return False, f'Maximum of {field.max}'
     return True, ''
 
-def validate_varchar(value, min: float = None, max: float = None) -> (bool, str):
+def validate_varchar(field, value) -> (bool, str):
     if not isinstance(value, str):
         return False, 'String only'
-    if min and len(value) < int(min):
-        return False, f'At least {int(min)} characters'
-    if max and len(value) > int(max):
-        return False, f'Up to {int(max)} characters'
+    if field.min and len(value) < int(field.min):
+        return False, f'At least {int(field.min)} characters'
+    if field.max and len(value) > int(field.max):
+        return False, f'Up to {int(field.max)} characters'
     return True, ''
 
-def validate_select(value, options: list) -> (bool, str):
-    if not options:
+def validate_select(field, value) -> (bool, str):
+    if not field.options:
         return False, 'No options to select from'
-    if type(value) != type(options[0]):
+    if type(value) != type(field.options[0]):
         return False, 'Type mismatch'
-    if value in options:
+    if value in field.options:
         return True, ''
     else:
-        return False, f'One of {options} only'
+        return False, f'One of {field.options} only'
 
--- tests/test_validators.py
+++ tests/test_validators.py
@@ -1,25 +1,31 @@
 import unittest
 from db import validator
+from db.model import Field
 
 class TestValidators(unittest.TestCase):
 
     def test_int_validator(self):
+        field = Field('test', Field.INTEGER_TYPE)
         correct = 548
-        self.assertTrue(validator.validate_int(correct)[0])
+        self.assertTrue(validator.validate_int(field, correct)[0])
         wrong = 'asd'
-        self.assertFalse(validator.validate_int(wrong)[0])
+        self.assertFalse(validator.validate_int(field, wrong)[0])
     
     def test_varchar_validator(self):
+        field = Field('test', Field.VARCHAR_TYPE)
         correct = 'some string'
-        self.assertTrue(validator.validate_varchar(correct, max=11)[0])
-        self.assertFalse(validator.validate_varchar(correct, max=5)[0])
-        self.assertFalse(validator.validate_varchar(correct, min=50)[0])
+        field.max = 11
+        self.assertTrue(validator.validate_varchar(field, correct)[0])
+        field.max = 5
+        self.assertFalse(validator.validate_varchar(field, correct)[0])
+        field.min = 50
+        self.assertFalse(validator.validate_varchar(field, correct)[0])
         wrong = 468
-        self.assertFalse(validator.validate_varchar(wrong)[0])
+        self.assertFalse(validator.validate_varchar(field, wrong)[0])
     
     def test_select_validator(self):
-        options = ['one', 'two']
-        self.assertTrue(validator.validate_select('one', options)[0])
-        self.assertTrue(validator.validate_select('two', options)[0])
-        self.assertFalse(validator.validate_select('five', options)[0])
-        self.assertFalse(validator.validate_select(5, options)[0])
+        field = Field('test', Field.SELECT_TYPE, options=['one', 'two'])
+        self.assertTrue(validator.validate_select(field, 'one')[0])
+        self.assertTrue(validator.validate_select(field, 'two')[0])
+        self.assertFalse(validator.validate_select(field, 'five')[0])
+        self.assertFalse(validator.validate_select(field, 5)[0])
