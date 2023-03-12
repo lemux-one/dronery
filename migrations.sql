977708bcff668db1e148e9f243586a08f86d22a6
Luis Lemus <lemux.one@gmail.com>
2023-03-12 15:19:24 -0400

    minor update to migrations script

--- migrations.sql
+++ migrations.sql
@@ -7,8 +7,8 @@ CREATE TABLE "drones" (
     "id" INTEGER PRIMARY KEY,
     "serial_number" VARCHAR(100) UNIQUE,
     "model" VARCHAR(20),
-    "weight" INTEGER,
+    "weight_limit" INTEGER,
     "battery_capacity" INTEGER,
     "state" VARCHAR(15)
 );
-INSERT INTO "drones" VALUES (NULL, "QWOIEJO1202115", "Lightweight", "250", "80", "IDLE");
+INSERT INTO "drones" VALUES (NULL, "QWOIEJO1202115", "Lightweight", 250, 80, "IDLE");
