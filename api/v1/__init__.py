403b19ebb06f0f6943d0db0d6ce92fa34bece7cd
Luis Lemus <lemux.one@gmail.com>
2023-03-12 20:03:29 -0400

    register medications handler in api.v1

--- api/v1/__init__.py
+++ api/v1/__init__.py
@@ -1,5 +1,7 @@
 from api.api_bottle import ApiBottle
 from .drones import handler as drones_handler
+from .medications import handler as medications_handler
 
 handler = ApiBottle()
 handler.mount('/drones', drones_handler)
+handler.mount('/medications', medications_handler)
