704cf7130d0f5d97d5b20074c9bb295c3439cf2f
Luis Lemus <lemux.one@gmail.com>
2023-03-13 11:48:35 -0400

    add periodic audit task

---
+++ audit_task.py
@@ -0,0 +1,23 @@
+from threading import Timer
+from api.utils import log
+from db.sqlite import SqliteHelper
+
+LOW_LEVEL = 25
+
+def run():
+    dbhelper = SqliteHelper()
+    ok, drones = dbhelper.query('select * from drones;')
+    if not ok:
+        log('Audit failed: Database query failed. See above logs for details.', 'ERROR')
+    else:
+        low_batt = 0
+        for drone in drones:
+            if drone['battery_capacity'] < LOW_LEVEL:
+                low_batt += 1
+        log(f'Audit report: Low battery for {low_batt}/{len(drones)} drones', 'AUDIT')
+    dbhelper.close()
+    Timer(5, run).start()
+
+
+if __name__ == "__main__":
+    run()
