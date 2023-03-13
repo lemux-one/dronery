b68ef0e101aa5be4d70fec1f2f3302b6b19b43cd
Luis Lemus <lemux.one@gmail.com>
2023-03-13 17:10:20 -0400

    expose test modules for easy CLI invocation

---
+++ tests/__init__.py
@@ -0,0 +1,6 @@
+from .test_sqlite_helper import TestSqliteHelper
+from .test_utils import TestUtils
+from .test_validators import TestValidators
+
+from .test_root_api import TestRootApi
+from .test_drones_handler import TestDronesHandler
