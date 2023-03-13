11269cea6a4d82da791998958a1290f53895913d
Luis Lemus <lemux.one@gmail.com>
2023-03-13 04:25:38 -0400

    register images handler in api.v1

--- api/v1/__init__.py
+++ api/v1/__init__.py
@@ -1,7 +1,9 @@
 from api.api_bottle import ApiBottle
 from .drones import handler as drones_handler
 from .medications import handler as medications_handler
+from .images import handler as images_handler
 
 handler = ApiBottle()
 handler.mount('/drones', drones_handler)
 handler.mount('/medications', medications_handler)
+handler.mount('/images', images_handler)
