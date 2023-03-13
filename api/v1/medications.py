4f0d614f851090dfb53c1eed3f73c5e6f0e857cd
Luis Lemus <lemux.one@gmail.com>
2023-03-13 05:08:24 -0400

    add regex constraints to name and code fields

--- api/v1/medications.py
+++ api/v1/medications.py
@@ -29,11 +29,11 @@ A Medication has:
 '''
 model = Model(table='medications')
 model.add_field(Field(name='medication_id', dtype=Field.INTEGER_TYPE, pk=True))
-model.add_field(Field(name='name', dtype=Field.VARCHAR_TYPE, 
-    max=100, unique=True))
+model.add_field(Field(name='name', dtype=Field.VARCHAR_TYPE, unique=True,
+    max=100, regex=r'^([a-z]|[A-Z]|\d|-|_)+$'))
 model.add_field(Field(name='weight', dtype=Field.INTEGER_TYPE, min=0))
-model.add_field(Field(name='code', dtype=Field.VARCHAR_TYPE, 
-    max=100, unique=True))
+model.add_field(Field(name='code', dtype=Field.VARCHAR_TYPE, unique=True,
+    max=100, regex=r'^([A-Z]|\d|_)+$'))
 model.add_field(Field(name='image_id', dtype=Field.INTEGER_TYPE, 
     fk=True, ftable='images'))
 
