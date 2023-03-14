75a1594d2479611643aa93df17a404cbda184174
Luis Lemus <lemux.one@gmail.com>
2023-03-14 15:13:52 -0400

    use double type for weight

--- api/v1/medications.py
+++ api/v1/medications.py
@@ -33,7 +33,7 @@ model = Model(table='medications')
 model.add_field(Field(name='medication_id', dtype=Field.INTEGER_TYPE, pk=True))
 model.add_field(Field(name='name', dtype=Field.VARCHAR_TYPE, unique=True,
     max=100, regex=r'^([a-z]|[A-Z]|\d|-|_)+$'))
-model.add_field(Field(name='weight', dtype=Field.INTEGER_TYPE, min=0))
+model.add_field(Field(name='weight', dtype=Field.DOUBLE_TYPE, min=0))
 model.add_field(Field(name='code', dtype=Field.VARCHAR_TYPE, unique=True,
     max=100, regex=r'^([A-Z]|\d|_)+$'))
 model.add_field(Field(name='image_id', dtype=Field.INTEGER_TYPE, 
