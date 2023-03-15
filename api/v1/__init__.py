9090c449e7079534aaaac36de4fa264a80b51361
Luis Lemus <lemux.one@gmail.com>
2023-03-15 12:37:47 -0400

    minor improvements for api discoverabilty

--- api/v1/__init__.py
+++ api/v1/__init__.py
@@ -9,3 +9,15 @@ handler.mount('/drones', drones_handler)
 handler.mount('/medications', medications_handler)
 handler.mount('/images', images_handler)
 handler.mount('/loads', loads_handler)
+
+@handler.get('/')
+def handle_root():
+    return '''<h1>API v1:</h1>
+    <h2>Available endpoints:</h2>
+    <ul>
+        <li><a href="drones">Drones</a></li>
+        <li><a href="medications">Medications</a></li>
+        <li><a href="images">Images</a></li>
+        <li><a href="loads">Loads</a></li>
+    </ul>
+    '''
